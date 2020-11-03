7af507c51090ca9db9b914fdf4d1178618c75cb7
Jack F. Murphy <me@jackmurphy.nyc>
2020-11-03 12:50:08 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,7 +6,7 @@ for ((i=0; i<${#md_files[@]}; i++)); do
     IFS='/' read -ra markdown_name <<< "${md_files[$i]}"
     IFS='.' read -ra base_name <<< "${markdown_name[1]}"
     echo "$base_name"
-    python3 -m markdown "${md_files[$i]}" >> pages/"$base_name".html
+    python3 -m markdown "${md_files[$i]}" > pages/"$base_name".html
 done
 
 
